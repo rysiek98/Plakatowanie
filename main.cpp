6db9acba98f9c51b8a591144ce06cf47ec057cb7
rysiek98 <52510889+rysiek98@users.noreply.github.com>
2021-04-08 23:25:11 +0200

    Program efficiency improved

--- main.cpp
+++ main.cpp
@@ -1,52 +1,79 @@
 #include <iostream>
 #include <fstream>
-#include <chrono>
 
 using namespace std;
 
-int buildingsNumber = 0,lastBuildingHeight = 0, result = 0;
+int amountOfBuildings = 0,lastBuildingHeight = 0, result = 0;
 int buildingsHeight[250000] = {0};
 int buildingsWidth[250000] = {0};
-int buildings[15000] = {0};
-int it=0;
-bool flag = false;
+int buildingsTable[250000] = {0};
+int lastBuildingNumber = 0;
 
 void addBuilding(int buildingHeight){
-    if(buildingHeight == buildings[it]){
+    if(buildingHeight == buildingsTable[lastBuildingNumber]){
         return;
     }
-    else if(buildingHeight > buildings[it]){
-        buildings[++it] = buildingHeight;
+    else if(buildingHeight > buildingsTable[lastBuildingNumber]){
+        buildingsTable[++lastBuildingNumber] = buildingHeight;
         return;
     }
-    for(int i = 0; i <= it; i++)
-    {
-        if(buildings[i] >= buildingHeight){
-            for(int j = i; j <= it; j++){
-                buildings[j] = 0;
-            }
-            buildings[i] = buildingHeight;
-            it=i;
-            return;
-        }
-    }
+
+//    for(int i = lastBuildingNumber; i >= 0; i--)
+//    {
+//        if(buildingsTable[i] < buildingHeight){
+//            buildingsTable[i + 1] = buildingHeight;
+//            lastBuildingNumber= i + 1;
+//            return;
+//        }
+//        if(i == 0 && buildingsTable[0] >= buildingHeight){
+//            buildingsTable[0] = buildingHeight;
+//            lastBuildingNumber=0;
+//            return;
+//        }
+//    }
 }
 
 int main() {
 
     //From keyboard
    /*
-    scanf("%d", &buildingsNumber);
-    for(int i = 0; i < buildingsNumber; i++){
+    scanf("%d", &amountOfBuildings);
+    for(int i = 0; i < amountOfBuildings; i++){
         scanf("%d %d", &buildingsWidth[i], &buildingsHeight[i]);
     }*/
 
    //From file
     fstream file;
-    file.open("..\\buildings.txt", ios::in);
+    //file.open("..\\tests_data\\in\\pla1a.in", ios::in);
+    //file.open("..\\tests_data\\in\\pla1b.in", ios::in);
+    //file.open("..\\tests_data\\in\\pla1c.in", ios::in);
+    //file.open("..\\tests_data\\in\\pla1ocen.in", ios::in);
+    //file.open("..\\tests_data\\in\\pla2a.in", ios::in);
+    //file.open("..\\tests_data\\in\\pla2b.in", ios::in);
+    //file.open("..\\tests_data\\in\\pla2c.in", ios::in);
+    //file.open("..\\tests_data\\in\\pla2ocen.in", ios::in);
+    //file.open("..\\tests_data\\in\\pla3a.in", ios::in);
+    //file.open("..\\tests_data\\in\\pla3b.in", ios::in);
+    //file.open("..\\tests_data\\in\\pla3c.in", ios::in);
+    //file.open("..\\tests_data\\in\\pla3ocen.in", ios::in);
+    //file.open("..\\tests_data\\in\\pla4a.in", ios::in);
+    //file.open("..\\tests_data\\in\\pla4b.in", ios::in);
+    //file.open("..\\tests_data\\in\\pla4ocen.in", ios::in);
+    //file.open("..\\tests_data\\in\\pla5a.in", ios::in);
+    //file.open("..\\tests_data\\in\\pla5b.in", ios::in);
+    //file.open("..\\tests_data\\in\\pla6a.in", ios::in);
+    //file.open("..\\tests_data\\in\\pla6b.in", ios::in);
+    //file.open("..\\tests_data\\in\\pla7a.in", ios::in);
+    //file.open("..\\tests_data\\in\\pla7b.in", ios::in);
+    //file.open("..\\tests_data\\in\\pla8a.in", ios::in);
+    //file.open("..\\tests_data\\in\\pla8b.in", ios::in);
+    //file.open("..\\tests_data\\in\\pla9a.in", ios::in);
+    //file.open("..\\tests_data\\in\\pla9b.in", ios::in);
+    //file.open("..\\tests_data\\in\\pla10a.in", ios::in);
+    file.open("..\\tests_data\\in\\pla10b.in", ios::in);
     if(file.good()){
-        file >> buildingsNumber;
-        for(int i = 0; i< buildingsNumber; i++){
+        file >> amountOfBuildings;
+        for(int i = 0; i < amountOfBuildings; i++){
             file >> buildingsWidth[i];
             file >> buildingsHeight[i];
         }
@@ -54,43 +81,45 @@ int main() {
         printf("Error!!");
     }
     file.close();
-    auto begin = std::chrono::high_resolution_clock::now();
+
     result = 1;
-    buildings[0] = buildingsHeight[0];
+    buildingsTable[0] = buildingsHeight[0];
     lastBuildingHeight = buildingsHeight[0];
 
-    for(int i = 1; i < buildingsNumber; i++){
+    for(int i = 1; i < amountOfBuildings; i++){
         if (lastBuildingHeight == buildingsHeight[i]){
-            //cout<<"Warunek 1 "<<buildingsHeight[i]<<endl;
             lastBuildingHeight = buildingsHeight[i];
-            continue;
         } else if(lastBuildingHeight < buildingsHeight[i]){
-            //cout<<"Warunek 2 "<<buildingsHeight[i]<<endl;
             result++;
             addBuilding(buildingsHeight[i]);
             lastBuildingHeight = buildingsHeight[i];
         } else{
-            for (int j = 0; j <= it; ++j) {
-                if(buildings[j] == buildingsHeight[i]){
-                    //cout<<"Warunek 3 "<<buildingsHeight[i]<<endl;
-                    addBuilding(buildingsHeight[i]);
-                    flag = true;
+            for (int j = lastBuildingNumber; j >= 0; j--) {
+
+                if(buildingsTable[j] == buildingsHeight[i]){
+                    lastBuildingNumber = j;
+                    break;
+                }
+
+                if(buildingsTable[j] < buildingsHeight[i]){
+                    buildingsTable[j + 1] = buildingsHeight[i];
+                    lastBuildingNumber= j + 1;
+                    result++;
+                    break;
+                }
+
+                if(j == 0 && buildingsTable[0] >= buildingsHeight[i]){
+                    buildingsTable[0] = buildingsHeight[i];
+                    lastBuildingNumber=0;
+                    result++;
                     break;
                 }
             }
-            if(!flag){
-                //cout<<"Warunek 4 "<<buildingsHeight[i]<<endl;
-                addBuilding(buildingsHeight[i]);
-                result++;
-            }
-            flag = false;
             lastBuildingHeight = buildingsHeight[i];
         }
 
     }
-    auto end = std::chrono::high_resolution_clock::now();
-    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
     printf("Wynik: %d\n",result);
-    printf("Time measured: %.3f seconds.\n", elapsed.count() * 1e-9);
     return 0;
 }
+
